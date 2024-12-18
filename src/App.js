e34c16304a1de100e407cac923977b8d05b74ea5
Atiwityano2541 <atiwit.ya@thebrainstem.com>
2024-12-19 00:29:16 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import BangkokMap from './Components/Mainmap';
 import BangkokMapboundary from './Components/Mainmap_v1';
-
+import Home from './Components/Home';
 
 function App() {
  
   return (
     <Router>
       <Routes>
-      <Route path="/" element={<BangkokMap />} /> 
+      <Route path="/" element={<Home />} /> 
         <Route path="/BangkokMap_boundary" element={<BangkokMapboundary />} /> 
         <Route path="/BangkokMap" element={<BangkokMap />} /> 
   
